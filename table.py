814e61952ab472e6605ec6a27dd092f8e5ba48e1
lucasgaleano <l.a.galeano2@gmail.com>
2019-09-20 14:31:54 -0300

    implemento checkWinner

--- table.py
+++ table.py
@@ -11,12 +11,21 @@ class Table():
 
 
     def start():
-        self.refresh()
         self.nextMove()
+        self.refresh()
+
+    def turn(self, player):
+        self.refresh()
+        player.move(self)
+        #self.checkWinner()
 
     def print(self):
         for i in range(3):
             print(str(self.table[i][0]) + '|' + str(self.table[i][1]) + '|' + str(self.table[i][2]))
+
+    def printWinner(self):
+        table.refresh()
+        print('Winner: ' + self.checkWinner())
 
     def refresh(self):
         os.system('reset')
@@ -35,3 +44,21 @@ class Table():
          for i in range(3):
              if ' ' in self.table[i]:
                 return (i+1)*3-(2-self.table[i].index(' '))
+
+    def checkWinner(self):
+        for i in range(3):
+            if self.checkColumn(i):
+                if self.table[i][0] == ' ':
+                    return None
+                return self.table[i][0]
+
+            if self.checkRow(i):
+                if self.table[0][i] == ' ':
+                    return None
+                return self.table[0][i]
+
+    def checkColumn(self, column):
+        return self.table[column][0]== self.table[column][1] and self.table[column][1] == self.table[column][2]
+
+    def checkRow(self, row):
+        return self.table[0][row]== self.table[1][row] and self.table[1][row] == self.table[2][row]
