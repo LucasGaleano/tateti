cd5f07f44cd5f37b43ea7e8c32c29dccd341170b
lucasgaleano <l.a.galeano2@gmail.com>
2019-09-20 15:33:20 -0300

    agrego tests

--- test.py
+++ test.py
@@ -5,21 +5,26 @@ from move import *
 
 
 def equals(expresion, result):
-    return expresion == result
+    return str(expresion == result)
 
+print('------------')
 print('test table')
 table = Table([['X',' ',' '],[' ',' ',' '],[' ',' ',' ']])
-print(equals(table.findFirstPositionFree(), 2))
+print('elige la segunda posicion: ' + equals(table.findFirstPositionFree(), 2))
 
 table = Table([[' ',' ','X'],[' ',' ',' '],[' ',' ',' ']])
-print(equals(table.findFirstPositionFree(), 1))
+print('elige la primera posicion: ' + equals(table.findFirstPositionFree(), 1))
 
 table = Table([['O','O','X'],[' ','X',' '],[' ',' ',' ']])
-print(equals(table.findFirstPositionFree(), 4))
-print(equals(table.checkWinner(), None))
+print('elige la cuarta posicion: ' + equals(table.findFirstPositionFree(), 4))
+
+print('------------')
+print('test winner')
+table = Table([['O','O','X'],[' ','X',' '],['O',' ',' ']])
+print('Sin ganador: ' + equals(table.checkWinner(), None))
 
 table = Table([['O','O','O'],[' ','X',' '],['O',' ',' ']])
-print(equals(table.checkWinner(), 'O'))
+print('ganador la O fila: ' + equals(table.checkWinner(), 'O'))
 
 table = Table([['O','X','X'],['O','X',' '],[' ','X',' ']])
-print(equals(table.checkWinner(), 'X'))
+print('ganador la X diagonal: ' + equals(table.checkWinner(), 'X'))
